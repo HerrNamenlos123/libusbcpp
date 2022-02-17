821dce9b85c7cc6971a78363a48b701487c8bb26
HerrNamenlos123 <herrnamenlos123@gmail.com>
2022-02-17 22:24:14 +0100

    Basic library setup

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # libusbcpp
 
 This library is an object-oriented C++ wrapper around the official libusb-1.0 library.
+
+## How to use
+
+This library can be included by a premake-script. Create a repository with a premake project.
+
+Add the library as a git submodule to your repository:  
+```
+git submodule add https://github.com/HerrNamenlos123/libusbcpp.git modules/libusbcpp
+```
+
+Then include it from `premake5.lua`:
+
+```lua
+-- Include the library
+include "modules/libusbcpp" -- Path to your submodule
+```
+
+This adds the project itself to the solution. After that use the following to link it to your project:
+
+```lua
+-- libusbcpp dependency
+dependson "libusbcpp"
+includedirs (LIBUSBCPP_INCLUDE_DIRS)
+libdirs (LIBUSBCPP_LINK_DIRS)
+links (LIBUSBCPP_LINKS)
+```
---
+++ premake5.lua
@@ -0,0 +1,66 @@
+
+-- Utility functions
+function appendTable(tableA, tableB)
+    for _,v in ipairs(tableB) do 
+        table.insert(tableA, v) 
+    end
+end
+
+-- Main library project
+project "libusbcpp"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    location "build"
+    targetdir "bin/%{cfg.buildcfg}"
+    targetname "%{prj.name}"
+    
+    filter { "platforms:x86" }
+        architecture "x86"
+
+    filter { "platforms:x64" }
+        architecture "x86_64"
+
+    filter "configurations:Debug"
+        defines { "DEBUG", "_DEBUG", "NDEPLOY" }
+        runtime "Debug"
+        symbols "On"
+        optimize "Off"
+
+    filter "configurations:Release"
+        defines { "NDEBUG", "NDEPLOY" }
+        runtime "Release"
+        symbols "On"
+        optimize "On"
+
+    filter "configurations:Deploy"
+        defines { "NDEBUG", "DEPLOY" }
+        runtime "Release"
+        symbols "Off"
+        optimize "On"
+
+    filter {}
+
+
+    -- Include directories
+    local _includedirs = { 
+        _SCRIPT_DIR .. "/include"
+    }
+    includedirs (_includedirs)
+
+    
+    -- Main source files
+    files ({ "include/**", "src/**" })
+
+
+
+
+    -- Include and linker information for premake projects using this library
+    LIBUSBCPP_INCLUDE_DIRS = {}
+    appendTable(LIBUSBCPP_INCLUDE_DIRS, _includedirs)
+
+    LIBUSBCPP_LINK_DIRS = {}
+    appendTable(LIBUSBCPP_LINK_DIRS, _SCRIPT_DIR .. "/bin/%{cfg.buildcfg}/")
+
+    LIBUSBCPP_LINKS = { "libusbcpp" }
